d41acfb743843c6021e4284f028079c7cca88d48
farhan-begg <farhan.begg@students.makeschool.com>
2021-10-02 16:10:05 -0700

    implemented sidebar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./screens/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Sidebar.css'
+const Sidebar = () => {
+    return (
+        <div className="sidebar_body">
+            <ul className="sidebar_titles">
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+            </ul>
+ 
+        </div>
+    )
+}
+
+export default Sidebar
